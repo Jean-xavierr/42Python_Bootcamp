167867128bd58160be9b4fec455260c7c42a669f
Jean-xavierr <religieuxjxavier@gmail.com>
2021-12-01 15:41:24 +0100

    :arrow_up: correction ex04

--- module00/ex04/operations.py
+++ module00/ex04/operations.py
@@ -1,6 +1,12 @@
 import sys
 
 
+def is_integer(nb: str) -> bool:
+    if nb.isdigit() or nb.startswith('-') and nb[1:].isdigit():
+        return True
+    return False
+
+
 def usage() -> None:
     print(f"""Usage: python operations.py <number1> <number2>
 Example:
@@ -17,14 +23,14 @@ Remainder:  {remainder}""")
 
 def operations(nb_one: int, nb_two: int) -> str:
     sum: int = nb_one + nb_two
-    difference: int = nb_one - nb_two if nb_one > nb_two else nb_two - nb_one
+    difference: int = nb_one - nb_two
     product: int = nb_one * nb_two
     if nb_two == 0:
         quotient = "Error (div by zero)"
         modulo = "Error (modulo by zero)"
     else:
-        quotient: int = nb_one / nb_two if nb_one > nb_two else nb_two / nb_one
-        modulo: int = nb_one % nb_two if nb_one > nb_two else nb_two % nb_one
+        quotient: int = nb_one / nb_two
+        modulo: int = nb_one % nb_two
     print_operations(sum, difference, product, quotient, modulo)
 
 
@@ -32,11 +38,14 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         usage()
     elif len(sys.argv) == 3:
-        if not sys.argv[1].isdigit() or not sys.argv[2].isdigit():
+        if not is_integer(sys.argv[1]) or not is_integer(sys.argv[2]):
             print("InputError: only numbers\n")
             usage()
         else:
             operations(int(sys.argv[1]), int(sys.argv[2]))
+    elif len(sys.argv) == 2:
+        print("InputError: not enough arguments\n")
+        usage()
     else:
         print("InputError: too many arguments\n")
         usage()
