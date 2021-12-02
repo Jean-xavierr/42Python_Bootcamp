da7ca6ccd27ca41b2035deb90152a882418469ef
Jean-xavierr <religieuxjxavier@gmail.com>
2021-12-02 14:11:04 +0100

    :arrow_up: correction ex10

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -9,10 +9,7 @@ def ft_progress(lst: list):
         pourcent = int(i * 100 / len_lst)
         bar_fill = int(len_bar * i // len_lst)
         bar = '=' * bar_fill + '>' + ' ' * (len_bar - bar_fill)
-        time_exec = int(time() - current_time)
-        if time_exec == 0:
-            time_exec = 1
-        time_left = time_exec / i * (len_lst - i)
+        time_left = (time() - current_time) / i * (len_lst - i)
         print("ETA: {:.2f}s [ {: 2d}%][{}] {: 4d}/{} | elapsed time {:.2f}s"
               .format(time_left, pourcent, bar, i,
                       len_lst, time() - current_time), end="\r")
@@ -20,7 +17,7 @@ def ft_progress(lst: list):
 
 
 if __name__ == '__main__':
-    listy = range(100)
+    listy = range(3000)
     ret = 0
     for elem in ft_progress(listy):
         ret += (elem + 3) % 5
